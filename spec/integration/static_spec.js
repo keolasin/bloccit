d521ddc27a0d004661eb90b764b6400d38e45c97
Keolasin <43076895+keolasin@users.noreply.github.com>
2019-06-09 21:39:41 -0700

    add marco route

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marco = "http://localhost:3000/marco";
 
 describe("routes: static", () => {
 
@@ -9,6 +10,22 @@ describe("routes: static", () => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
 
+        done();
+      });
+    });
+  });
+
+  describe("GET /marco", () => {
+    it("should return status code 200", (done) => {
+      request.get(marco, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+    });
+
+    it("should return response of 'polo'", (done) => {
+      request.get(marco, (err, res, body) => {
+        expect(body).toBe("polo");
         done();
       });
     });
