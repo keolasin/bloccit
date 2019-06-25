10716dfb43bde6e962ab4870439d5fe89727da58
Keolasin <43076895+keolasin@users.noreply.github.com>
2019-06-25 13:48:56 -0700

    remove duplicate getPoints() method from Post model

--- spec/integration/favorites_spec.js
+++ spec/integration/favorites_spec.js
@@ -153,9 +153,7 @@ describe("routes : favorites", () => {
             favCountBeforeDelete = favorites.length;
 
             request.post(
-              `${base}${this.topic.id}/posts/${this.post.id}/favorites/${
-                favorite.id
-              }/destroy`,
+              `${base}${this.topic.id}/posts/${this.post.id}/favorites/${favorite.id}/destroy`,
               (err, res, body) => {
                 this.post.getFavorites().then(favorites => {
                   expect(favorites.length).toBe(favCountBeforeDelete - 1);
